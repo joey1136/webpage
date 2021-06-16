5d32b92f3ce1592a22f651a0ae33287c91041ef4
joey kwok <joey.kwok@pastellink.com>
2021-06-16 18:28:27 +0800

    add card in home page

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,3 +1,41 @@
-export function HomePage() {
-  return <div>Hello This is Home Page</div>;
-}
+import React from "react";
+import styled from "styled-components";
+import { Button, Card, Image } from "semantic-ui-react";
+
+const Root = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  background-color: papayawhip;
+  justify-content: center;
+  align-items: center;
+`;
+const Content = styled.div``;
+
+const Introduction = styled.div``;
+
+const Name = styled.div``;
+
+export const HomePage: React.FunctionComponent = () => {
+  return (
+    <Root>
+      <Content>
+        <Card>
+          <Image src="../public/logo192.png" />
+          <Card.Content>
+            <Card.Header>
+              <Name>Joey Kwok</Name>
+            </Card.Header>
+            <Card.Meta>
+              <Introduction>Hello This is Home Page</Introduction>
+            </Card.Meta>
+            <Card.Description>IT Fresh Grad Student</Card.Description>
+          </Card.Content>
+          <Card.Content>
+            <Button>Profile</Button>
+          </Card.Content>
+        </Card>
+      </Content>
+    </Root>
+  );
+};
